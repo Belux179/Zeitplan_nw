9cf8141d6a2011f1f45e17b0d1bc17b9f0149ac0
Belux179 <ebls97@gmail.com>
2022-07-18 11:01:09 -0600

    incorporar boostrap

--- webapp/urls.py
+++ webapp/urls.py
@@ -4,6 +4,8 @@ from .views import *
 
 urlpatterns = [
     path('', home, name='home'),
-    
+    path('aula/', aula, name='aula'),
+    path('grado/', grado, name='grado'),
+    path('profesor/', profesor, name='profesor'),
 ]
 
--- webapp/views.py
+++ webapp/views.py
@@ -2,5 +2,18 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def home(request):
     return render(request, 'web/home.html')
+
+
+def aula(request):
+    return render(request, 'web/aula.html')
+
+
+def grado(request):
+    return render(request, 'web/grado.html')
+
+
+def profesor(request):
+    return render(request, 'web/profesor.html')
